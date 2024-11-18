9ad95462a0ff0b79748d6b254bec8db9e553390a
Josh Holbrook <josh.holbrook@gmail.com>
2024-11-17 19:00:46 -0900

    API client docstrings and pagination helper

--- tfmod/api/__init__.py
+++ tfmod/api/__init__.py
@@ -1,7 +1,18 @@
 # https://developer.hashicorp.com/terraform/registry/api-docs
 
 from dataclasses import dataclass
-from typing import Any, Dict, List, Optional, Protocol, Set
+from typing import (
+    Any,
+    Callable,
+    cast,
+    Dict,
+    Generator,
+    List,
+    Optional,
+    Protocol,
+    Set,
+    TypeVar,
+)
 
 import requests
 
@@ -357,7 +368,14 @@ def raise_for_status(res: requests.Response, codes: Optional[Set[int]] = None) -
 
 
 class APIClient:
-    base_url = "https://registry.terraform.io/v1/modules"
+    """
+    An API client for the Terraform registry.
+    """
+
+    def __init__(
+        self, base_url: str = "https://registry.terraform.io/v1/modules"
+    ) -> None:
+        self.base_url = base_url
 
     def list(
         self,
@@ -366,6 +384,10 @@ class APIClient:
         provider: Optional[str] = None,
         verified: bool = False,
     ) -> ModuleList:
+        """
+        List modules under a namespace.
+        """
+
         url = f"{self.base_url}/{namespace}" if namespace else self.base_url
 
         params: Dict[str, str] = dict()
@@ -395,6 +417,10 @@ class APIClient:
         namespace: Optional[str] = None,
         verified: bool = False,
     ) -> ModuleList:
+        """
+        Search for modules.
+        """
+
         url = f"{self.base_url}/search"
 
         params: Dict[str, str] = dict(q=q)
@@ -420,6 +446,10 @@ class APIClient:
         )
 
     def versions(self, namespace: str, name: str, provider: str) -> VersionList:
+        """
+        Get versions for a module given a provider.
+        """
+
         url = f"{self.base_url}/{namespace}/{name}/{provider}/versions"
 
         res = requests.get(url)
@@ -430,25 +460,13 @@ class APIClient:
 
         return VersionList.from_json(data)
 
-    def _download(self, url: str) -> str:
-        res = requests.get(url, allow_redirects=True)
-
-        raise_for_status(res, {204})
-
-        get: Optional[str] = res.headers["x-terraform-get"]
-
-        if not get:
-            raise ValueError("No download URL")
-
-        return get
-
-    def download(self, namespace: str, name: str, provider: str, version: str) -> str:
-        url = f"{self.base_url}/{namespace}/{name}/{provider}/{version}/download"
-        return self._download(url)
-
     def latest(
         self, namespace: str, name: str, offset: Optional[int] = None
     ) -> ModuleList:
+        """
+        Get the latest versions of a module.
+        """
+
         url = f"{self.base_url}/{namespace}/{name}"
 
         params: Dict[str, str] = dict()
@@ -465,6 +483,10 @@ class APIClient:
         return ModuleList.from_json(data)
 
     def latest_for_provider(self, namespace: str, name: str, provider: str) -> Module:
+        """
+        Get the latest version of a module for a provider.
+        """
+
         url = f"{self.base_url}/{namespace}/{name}/{provider}"
 
         res = requests.get(url)
@@ -476,6 +498,10 @@ class APIClient:
         return Module.from_json(data)
 
     def get(self, namespace: str, name: str, provider: str, version: str) -> Module:
+        """
+        Get a module for a specific provider and version.
+        """
+
         url = f"{self.base_url}/{namespace}/{name}/{provider}/{version}"
 
         res = requests.get(url)
@@ -486,12 +512,41 @@ class APIClient:
 
         return Module.from_json(data)
 
-    def download_latest(self, namespace: str, name: str, provider: str):
+    def _download(self, url: str) -> str:
+        res = requests.get(url, allow_redirects=True)
+
+        raise_for_status(res, {204})
+
+        get: Optional[str] = res.headers["x-terraform-get"]
+
+        if not get:
+            raise ValueError("No download URL")
+
+        return get
+
+    def download_url(
+        self, namespace: str, name: str, provider: str, version: str
+    ) -> str:
+        """
+        Get the download URL for a specific version of a module.
+        """
+        url = f"{self.base_url}/{namespace}/{name}/{provider}/{version}/download"
+        return self._download(url)
+
+    def latest_download_url(self, namespace: str, name: str, provider: str):
+        """
+        Get the download URL for the latest version of a module.
+        """
+
         url = f"{self.base_url}/{namespace}/{name}/{provider}/download"
 
         return self._download(url)
 
     def metrics(self, namespace: str, name: str, provider: str) -> Summary:
+        """
+        Get download metrics for a module and provider.
+        """
+
         url = f"{self.base_url}/{namespace}/{name}/{provider}/downloads/summary"
 
         res = requests.get(url)
@@ -501,3 +556,21 @@ class APIClient:
         data = res.json()
 
         return Summary.from_json(data)
+
+
+T = TypeVar("T", bound=Paginated)
+Method = Callable[..., T]
+
+
+def paginate(method: Method[T], *args: Any, **kwargs: Any) -> Generator[T, None, None]:
+    """
+    Iterate over a paginated method on APIClient.
+    """
+
+    res: T = method(*args, **kwargs)
+
+    while res.meta.next_offset is not None:
+        yield res
+        res = method(*args, **dict(kwargs, offset=res.meta.next_offset))
+
+    yield res
