2219de7058b2d55d361549cb041eacfaeca13cae
Josh Holbrook <josh.holbrook@gmail.com>
2024-11-26 10:20:20 -0900

    Warnings when loading module

--- tfmod/module.py
+++ tfmod/module.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from dataclasses import dataclass
 import os
 from pathlib import Path
@@ -5,10 +6,46 @@ from typing import Any, Dict, List, Optional, Self, Type
 
 import hcl2
 
+from tfmod.logging import logger
+
 # Make the type checker happy
 hcl: Any = hcl2
 
 Script = List[str]
+
+
+def warn_value(variable: str, type: str) -> None:
+    logger.warn(
+        f"Invalid value for {variable}",
+        f"""This value is not compatible with the module's type
+constraint: a {type} is required.""",
+    )
+
+
+class WarnScript:
+    def __init__(self) -> None:
+        self._warned: Dict[str, bool] = defaultdict(lambda: False)
+
+    def __call__(self, name: str) -> None:
+        if self._warned[name]:
+            return
+        self._warned[name] = True
+        warn_value(f"{name} script", "list(string)")
+
+
+MODULE_TYPE = """
+object({
+  name = string,
+  provider = string,
+  version = string,
+  description = string,
+  scripts = map(list(string))
+})
+""".replace(
+    "\n", ""
+).replace(
+    ",", ", "
+)
 
 
 @dataclass
@@ -23,16 +60,33 @@ class Module:
     @classmethod
     def load(cls: Type[Self], path: Path = Path(os.getcwd())) -> Self:
         with open(path / "module.tfvars", "r") as f:
-            var = hcl.load(f).get("module", dict())
+            var = hcl.load(f).get("module", None)
+
+        if not var:
+            warn_value("module", MODULE_TYPE)
+            return cls(
+                name=None, provider=None, version=None, description=None, scripts=dict()
+            )
 
         scripts: Dict[str, Script] = dict()
 
+        warn_script = WarnScript()
+
         if "scripts" in var:
-            scripts = var.scripts
-            assert type(scripts) == dict
-            for name, script in scripts.items():
-                assert type(script) == list
-                scripts[name] = script
+            defined_scripts = var["scripts"]
+            if type(defined_scripts) != dict:
+                warn_value("scripts", "map")
+            else:
+                for name, defined_script in defined_scripts.items():
+                    if type(defined_script) != list:
+                        warn_script(name)
+                    else:
+                        script: List[str] = list()
+                        for line in defined_script:
+                            if type(line) != str:
+                                warn_script(name)
+                            script.append(str(line))
+                        scripts[name] = script
 
         return cls(
             name=var.get("name", None),
