3c7e92b9e275f92f84adaec1d2a58e22ef529995
Josh Holbrook <josh.holbrook@gmail.com>
2024-11-30 13:50:31 -0900

    Validate version format

--- modules/spec/variables.tf
+++ modules/spec/variables.tf
@@ -11,4 +11,8 @@ variable "module" {
       main_branch = optional(string)
     }))
   })
+  validation {
+    condition = len(regexall("\\d+\\.\\d+\\.\\d+", "1.2.3")) > 0
+    error_message = "The version must follow simplified semver (ie. 1.2.3)"
+  }
 }
