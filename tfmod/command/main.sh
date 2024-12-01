6207d7c47fa7bcd9272706b92345eecd7bd5ce30
Josh Holbrook <josh.holbrook@gmail.com>
2024-11-30 18:21:32 -0900

    Fix cd bug in entrypoint

--- tfmod/command/main.sh
+++ tfmod/command/main.sh
@@ -1,4 +1,4 @@
-if ! cd ~/.local/state/tfmod || ! [ -d .venv ]; then
+if ! (cd ~/.local/state/tfmod && [ -d .venv ]); then
   fatal 'TfMod is not installed correctly' \
         'To install TfMod, run "tfmod update"'
 fi
@@ -6,5 +6,5 @@ fi
 # rather than using uv, just use the virtualenv directly...
 
 # shellcheck disable=SC1091
-source .venv/bin/activate
+source ~/.local/state/tfmod/.venv/bin/activate
 exec python3 -m tfmod "${ARGV[@]}"
