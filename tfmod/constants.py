710cb3212ad46f9d6201060e5c296848578947c7
Josh Holbrook <josh.holbrook@gmail.com>
2024-11-30 17:43:21 -0900

    Parse git remotes

--- tfmod/constants.py
+++ tfmod/constants.py
@@ -22,10 +22,18 @@ with open(PACKAGE_DIR / "pyproject.toml", "r") as f:
 STATE_DIR: Path = Path(os.path.expanduser("~/.local/state/tfmod"))
 
 _terraform_bin = which("terraform")
+_git_bin: str | None = which("git")
+_gh_bin: str | None = which("gh")
 
 if _terraform_bin is None:
     raise Error('"terraform" could not be found.')
+if _git_bin is None:
+    raise Error('"git" could not be found.')
+if _gh_bin is None:
+    raise Error('"gh" could not be found.')
 
 TERRAFORM_BIN: str = _terraform_bin
+GIT_BIN: str = _git_bin
+GH_BIN: str = _gh_bin
 
 GH_CONFIG_DIR: Path = Path(os.path.expanduser("~/.config/gh/"))
--- tfmod/git.py
+++ tfmod/git.py
@@ -1,5 +1,48 @@
 from dataclasses import dataclass
-from typing import Dict, Optional
+import os
+import re
+import subprocess
+from typing import Dict, Literal, Optional
+
+from tfmod.constants import GIT_BIN
+from tfmod.error import GitError
+
+Direction = Literal["fetch"] | Literal["push"]
+
+
+@dataclass
+class GitRemote:
+    # NOTE: It may be possible that either of these is optional. But I don't
+    # see any documentation for that in git and haven't seen it in practice.
+    fetch_url: str
+    push_url: str
+
+    def parse(self, direction: Direction = "push"):
+        url = self.push_url if direction == "push" else self.fetch_url
+        raise NotImplementedError("GitRemote#parse()")
+
+
+def git_remote(path: str = os.getcwd()):
+    proc = subprocess.run([GIT_BIN, "remote", "-v"], cwd=path, capture_output=True)
+    print(proc.stderr)
+    try:
+        proc.check_returncode()
+    except subprocess.CalledProcessError as exc:
+        raise GitError(str(exc))
+    else:
+        if proc.stderr:
+            print(proc.stderr)
+    out = proc.stdout.decode("unicode_escape")
+
+    remotes: Dict[str, Dict[str, str]] = dict()
+    for line in out.split("\n"):
+        name, url, direction = re.split(r"\s+", line)
+        direction = direction[1:-1]
+        if name not in remotes:
+            remotes[name] = dict()
+        remotes[name][f"{direction}_url"] = url
+
+    return {name: GitRemote(**kwargs) for name, kwargs in remotes.items()}
 
 
 @dataclass
